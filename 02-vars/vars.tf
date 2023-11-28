b16988eb3d4233b7c2b1dd880389371fd01e64a1
Abhishektodupunuri3 <abhishektodupunuri1698@gmail.com>
2023-11-28 15:55:07 -0600

    Auto Update

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -31,3 +31,9 @@ variable "example_list" {
         "Terraform"
     ]
 }
+
+# Try to understand here, the data in the list doesn't necessary to be of same data type. 
+
+output "example_list_op" {
+    value = "Welcome to ${var.example_list[1]}  with ${var.example_list[0]} Training and this is our batch ${var.example_list[2]} and the current topic is ${var.example_list[4]}"
+}
