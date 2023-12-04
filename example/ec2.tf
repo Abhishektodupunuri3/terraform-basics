9a7e0c7a5067066c5ed9c55be46245914baeeec7
Abhishektodupunuri3 <abhishektodupunuri1698@gmail.com>
2023-12-04 15:43:40 -0600

    Auto Update

--- example/ec2.tf
+++ example/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
-    ami             = "ami-05d263152e74d2910"
-    instance_type   = "t2.nano"
+    ami             = "ami-086e7126e24c09b4c"
+    instance_type   = "t2.micro"
 
     tags  ={
         Name = "TerrafromServer"
