eeec7cd7f9d77b1cf982a79382b84adfd21e56dc
Abhishektodupunuri3 <abhishektodupunuri1698@gmail.com>
2024-01-08 10:11:39 -0500

    Auto Push

--- 07-roboshop-setup/main.tf
+++ 07-roboshop-setup/main.tf
@@ -6,20 +6,20 @@ module "frontend" {
     APP_VERSION = each.value.APP_VERSION
 }
 
-# module "mongodb" {
-#     source      = "./ec2"
-#     COMPONENT   = "mongodb"
-#     APP_VERSION = "0.0.2"
-# }
+ module "mongodb" {
+     source      = "./ec2"
+     COMPONENT   = "mongodb"
+     APP_VERSION = "0.0.2"
+ }
 
-# module "cart" {
-#     source      = "./ec2"
-#     COMPONENT   = "cart"
-#     APP_VERSION = "0.0.2"
-# }
+ module "cart" {
+     source      = "./ec2"
+     COMPONENT   = "cart"
+     APP_VERSION = "0.0.2"
+ }
 
-# module "catalogue" {
-#     source      = "./ec2"
-#     COMPONENT   = "catalogue"
-#     APP_VERSION = "0.0.2"
-# }
+ module "catalogue" {
+     source      = "./ec2"
+     COMPONENT   = "catalogue"
+     APP_VERSION = "0.0.2"
+ }
